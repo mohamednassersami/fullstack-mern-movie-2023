d300624728d2dd915cc6bc00386adc4dc1bb549e
mohamednasser <mohamed.nasser.sami.97@gmail.com>
2023-10-31 19:39:43 +0200

    first commit

---
+++ src/redux/store.js
@@ -0,0 +1,18 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userSlice from "./features/userSlice";
+import themeModeSlice from "./features/themeModeSlice";
+import authModalSlice from "./features/authModalSlice";
+import appStateSlice from "./features/appStateSlice";
+import globalLoadingSlice from "./features/globalLoadingSlice";
+
+const store = configureStore({
+  reducer: {
+    user: userSlice,
+    themeMode: themeModeSlice,
+    authModal: authModalSlice,
+    globalLoading: globalLoadingSlice,
+    appState: appStateSlice,
+  },
+});
+
+export default store;
